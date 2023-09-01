141b3791204dc1b0ca44c3af3c53847d236bed19
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-09-01 11:35:22 +0530

    LinkedListImplementation

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    internal class LinkedList<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        public void AddFirst(T Data)
+        {
+            Node<T> newNode = new Node<T>(Data);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                newNode.next = head;
+                head = newNode;
+            }
+        }
+
+        public void AddLast(T Data)
+        {
+            Node<T> newNode = new Node<T>(Data);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        public void DeleteFirst()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("No Data Avaliable");
+            }
+            else
+            {
+                Console.WriteLine(head.Data + "  deleted successfully");
+                head = head.next;
+            }
+        }
+
+        public void DeleteLast()
+        {
+
+            if (head == null)
+            {
+                Console.WriteLine("No Data Avaliable");
+            }
+            if (head == tail)
+            {
+                Console.WriteLine(head.Data + " Deleted successfully");
+                head = null;
+            }
+
+            Node<T> TempNode = head;
+            while (TempNode.next != tail)
+            {
+                TempNode = TempNode.next;
+
+            }
+            if (TempNode.next == tail)
+            {
+                Console.WriteLine(tail.Data + " Deleted successfully");
+                TempNode.next = null;
+                tail = TempNode;
+            }
+        }
+        public void InsertAfter(T data, T search)
+        {
+            Node<T> newNode = new Node<T>(data);
+            Node<T> temp = head;
+            Node<T> temp2 = head;
+            while (temp != null && temp2 != null)
+            {
+                if (temp.Data.Equals(search))
+                {
+                    temp2 = temp2.next;
+                    temp.next = newNode;
+                    newNode.next = temp2;
+                    Console.WriteLine("Data inserted successfully!!");
+                }
+                temp = temp.next;
+                temp2 = temp2.next;
+            }
+        }
+
+        public void Display()
+        {
+            Node<T> temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.Data + " -> ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*LinkedList<String> Student = new LinkedList<String>();
+
+            // Adding elements in the LinkedList
+            // Using AddLast() method
+            Student.AddLast("Zoya");
+            Student.AddLast("Shilpa");
+            Student.AddLast("Rohit");
+            Student.AddLast("Rohan");
+            Student.AddLast("Rani");
+            Student.AddLast("Rohith");
+
+            Console.WriteLine("Students are :");
+
+         
+            foreach (string str in Student)
+            {
+                Console.WriteLine(str);
+            }
+
+
+            // Check if the given element is available or not
+
+            if (Student.Contains("Shilpa") == true)
+            {
+                Console.WriteLine("Element Found...!!");
+            }
+            else
+            {
+                Console.WriteLine("Element Not found...!!");
+            }
+
+            Console.WriteLine("Removing first element using RemoveFirst() method");
+
+            Student.RemoveFirst();
+
+            foreach (string str in Student)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine("Removing Last element using RemoveLast() method");
+
+            Student.RemoveLast();
+
+            foreach (string str in Student)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine("Removing Last element using RemoveLast() method");
+
+            var n = Student.AddFirst("Sai");
+            Student.AddBefore(n, "Rohith");
+            foreach (var d in Student)
+            {
+                Console.WriteLine(d);
+            }*/
+
+
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddFirst("A");
+            list.AddFirst("B");
+            list.AddFirst("C");
+            list.AddFirst("D");
+            list.AddFirst("E");
+
+            list.Display();
+            Console.WriteLine();
+            list.AddLast("z");
+            list.Display();
+            Console.WriteLine();
+            list.DeleteFirst();
+            list.Display();
+            Console.WriteLine();
+            list.DeleteLast();
+            list.Display();
+            Console.WriteLine();
+            list.Display();
+            Console.WriteLine();
+            list.InsertAfter("Vamshi", "C");
+            list.Display();
+        }
+    }
+    }
+
